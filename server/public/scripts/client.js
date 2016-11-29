c0552dee38b1c5756f75ca42dadf729019aae7ae
Liz Swabey-Keith <lizswabey-keith@Lizs-MacBook-Pro-2.local>
2016-11-29 14:38:29 -0600

    pictures of pets

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -27,9 +27,9 @@ app.config(['$routeProvider', function($routeProvider) {
 
 
 
-app.controller('firstController', ['$http', function($http) {
-  console.log('firstController up and running');
-  var key = 'b900e0d5e332753a460a64eaa8de00fd';
+app.controller('CatsController', ['$http', function($http) {
+  console.log('CatsController up and running');
+  var key = '94751f2782a0366a89fbd644a5e9c3ce';
   var self = this;
 
   self.animal = {};
@@ -38,6 +38,67 @@ app.controller('firstController', ['$http', function($http) {
     var query = 'http://api.petfinder.com/'; // baseURL for API
     query += 'pet.getRandom'; // selecting the method we would like to return
     query += '?key=' + key; // Giving petfinder our key
+    query += '&animal=cat'
+    query += '&format=json'; // Telling petfinder we want our response to be JSON
+    query += '&output=basic'; // Telling petfinder what data we want (more than just id)
+    var request = encodeURI(query) + '&callback=JSON_CALLBACK'; // removing spaces and special characters from response as well as making jsonp work with the callback
+
+    console.log('Request:', request);
+
+    $http.jsonp(request).then(function(response){
+      console.log(response);
+      self.animal = response.data.petfinder.pet;
+    });
+
+  }
+
+  self.getRandomPet();
+
+  self.message = "Welcome!"
+}]);
+
+app.controller('DogsController', ['$http', function($http) {
+  console.log('DogsController up and running');
+  var key = '94751f2782a0366a89fbd644a5e9c3ce';
+  var self = this;
+
+  self.animal = {};
+
+  self.getRandomPet = function(){
+    var query = 'http://api.petfinder.com/'; // baseURL for API
+    query += 'pet.getRandom'; // selecting the method we would like to return
+    query += '?key=' + key; // Giving petfinder our key
+    query += '&animal=dog'
+    query += '&format=json'; // Telling petfinder we want our response to be JSON
+    query += '&output=basic'; // Telling petfinder what data we want (more than just id)
+    var request = encodeURI(query) + '&callback=JSON_CALLBACK'; // removing spaces and special characters from response as well as making jsonp work with the callback
+
+    console.log('Request:', request);
+
+    $http.jsonp(request).then(function(response){
+      console.log(response);
+      self.animal = response.data.petfinder.pet;
+    });
+
+  }
+
+  self.getRandomPet();
+
+  self.message = "Welcome!"
+}]);
+
+app.controller('BirdsController', ['$http', function($http) {
+  console.log('BirdsController up and running');
+  var key = '94751f2782a0366a89fbd644a5e9c3ce';
+  var self = this;
+
+  self.animal = {};
+
+  self.getRandomPet = function(){
+    var query = 'http://api.petfinder.com/'; // baseURL for API
+    query += 'pet.getRandom'; // selecting the method we would like to return
+    query += '?key=' + key; // Giving petfinder our key
+    query += '&animal=bird'
     query += '&format=json'; // Telling petfinder we want our response to be JSON
     query += '&output=basic'; // Telling petfinder what data we want (more than just id)
     var request = encodeURI(query) + '&callback=JSON_CALLBACK'; // removing spaces and special characters from response as well as making jsonp work with the callback
